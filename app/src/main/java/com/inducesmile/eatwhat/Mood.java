6f11e430717d82451ea4fe9f98de37cc9a135120
Zora <a121622855@gmail.com>
2016-07-27 13:33:49 +0800

    心情+Toolbar+support.v7匯入

--- app/src/main/java/com/inducesmile/eatwhat/Mood.java
+++ app/src/main/java/com/inducesmile/eatwhat/Mood.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
 
 public class Mood extends AppCompatActivity {
 
@@ -12,6 +13,13 @@ public class Mood extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+
+        Toolbar my_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+
+        getSupportActionBar().setTitle(R.string.my_tb_title);
+       /* getSupportActionBar().setSubtitle(R.string.my_tb_subtitle);*/
+        getSupportActionBar().setIcon(R.drawable.ictoolbar);
 
         anImagebutton = (ImageButton) findViewById(R.id.ibNext);
         anImagebutton.setOnClickListener(new View.OnClickListener() {
